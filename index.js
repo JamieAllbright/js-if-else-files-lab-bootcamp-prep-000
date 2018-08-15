7d855831ac887e6ed6015ab5f29e67dd665050ab
Jamie Allbright <jamie.allbright@aol.com>
2018-08-15 21:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var name = "Joe"
 var height = "74"
 
-
+if ( name === "Susan") {
+  console.log ("The name is incorrect")
+} else { 
+  console.log("Expected name to be Susan")
+}   
 // Don't worry about this
 module.exports = { name, height
 }
